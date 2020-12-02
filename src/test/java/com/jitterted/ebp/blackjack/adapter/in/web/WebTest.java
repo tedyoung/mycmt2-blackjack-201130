e7b23b39f403007a6ed9f343f54ff5b054884d1f
Ted M. Young <tedyoung@gmail.com>
2020-12-01 20:05:04 -0800

    Implemented initial deal of cards to start the game.

---
+++ src/test/java/com/jitterted/ebp/blackjack/adapter/in/web/WebTest.java
@@ -0,0 +1,31 @@
+package com.jitterted.ebp.blackjack.adapter.in.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class WebTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  public void getOfHomePageIsStatus200() throws Exception {
+    mockMvc.perform(get("/index.html"))
+           .andExpect(status().is2xxSuccessful());
+  }
+
+  @Test
+  public void postToStartGamePageIs200() throws Exception {
+    mockMvc.perform(post("/start-game"))
+           .andExpect(status().is2xxSuccessful());
+  }
+
+
+}
